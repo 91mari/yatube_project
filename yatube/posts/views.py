28f8005d13338a66a6e6a5c55be1df3fd4b86a9e
мария артамонова <91.mari.artamonova@mail.ru>
2022-01-28 23:55:55 +0300

    commit

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,20 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import Post, Group
-# import os
 
 
 def index(request):
-    # template = 'posts/index.html'
-    # title = "Это главная страница проекта Yatube"
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        # В словарь можно передать переменную
-        # 'title': title,
         'posts': posts,
     }
     return render(request, 'posts/index.html', context)
-    # return render(request, 'posts/index.html')
 
 
 def group_posts(request, slug):
@@ -23,19 +17,17 @@ def group_posts(request, slug):
     # В нашем случае в переменную group будут переданы объекты модели Group,
     # поле slug у которых соответствует значению slug в запросе,
     group = get_object_or_404(Group, slug=slug)
+    NUM_OF_OBJ = 10
 
     # Метод .filter позволяет ограничить поиск по критериям.
     # Это аналог добавления
     # условия WHERE group_id = {group_id}
+    # posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    # posts = group.posts.all()[:NUM_OF_OBJ]
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
         'group': group,
         'posts': posts,
+        # 'title': f'записи сообщества <{group}>'
     }
-    # title = "Здесь будет информация о группах проекта Yatube"
-    # context = {
-    #     # В словарь можно передать переменную
-    #     'title': title,
-    # }
-    # template = 'posts/group_list.html'
     return render(request, 'posts/group_list.html', context)
